222804203329d899e12f57f68cb6e56cb46a4800
Micky Simwenyi <msimwenyi@uchicago.edu>
2015-01-12 21:14:33 -0600

    Create  solution_8.py

---
+++ solution_8.py
@@ -0,0 +1,16 @@
+# Week-1
+
+#Homwork 1
+#This program calculates the temperature in Celsius when Fahrenheit inputted
+
+def main():
+
+    print ("This Program Provides The Temp in Celsius When The Fahrenheit is inputted")
+
+    C = float(input("Please Enter The Temp in Celsius: "))
+    F = (C-32)/1.8
+
+    print("The Temp in Celsius is: ", F)
+
+
+main()
